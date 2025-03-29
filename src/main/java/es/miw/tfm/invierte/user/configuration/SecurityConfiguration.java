416acdb2821e7a9ac3933f88f698bec92c038a06
Denilsson Montoya Neyra <dmontoyane@gmail.com>
2025-03-29 23:13:59 +0100

    Fix Sonar Issues InvierteIO/betca-tfm#34

--- src/main/java/es/miw/tfm/invierte/user/configuration/SecurityConfiguration.java
+++ src/main/java/es/miw/tfm/invierte/user/configuration/SecurityConfiguration.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -18,7 +19,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -61,11 +61,12 @@ public class SecurityConfiguration {
             .build();
       }
 
-      throw new UsernameNotFoundException("User not found");
+      throw new BadCredentialsException("Bad credentials");
     };
   }
 
   @Bean
+  @SuppressWarnings("java:S4502")
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
         .csrf(AbstractHttpConfigurer::disable)
